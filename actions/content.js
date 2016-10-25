fbd01f1f7569aece5b85169cd67074075469de57
Gabriel Lundgren <glundgren93@gmail.com>
2016-10-25 15:40:52 -0200

    refactoring components and actions

--- actions/content.js
+++ actions/content.js
@@ -3,14 +3,10 @@ import axios from 'axios';
 
 export const ROOT_URL = 'https://social-projects-fbe1f.firebaseio.com/projects.json';
 
-const setContent = (contents) => {
-  console.log(contents);
-
-  return {
+const setContent = (contents) => ({
     type: actionTypes.CONTENT_SET,
     contents
-  };
-};
+});
 
 
 export const getContent = () => {
--- components/Content/index.js
+++ components/Content/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as actions from '../../actions';
+import { getContent } from '../../actions';
 import Content from './presenter';
 
-const mapStateToProps = (state) => {
-  const contents = state.content;
+const mapStateToProps = (state) => ({
+    contents: state.content
+})
 
-  return {
-    contents
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getContent: bindActionCreators(actions.getContent, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Content);
+export default connect(
+  mapStateToProps,
+  { getContent } // injects getContent
+)(Content);
