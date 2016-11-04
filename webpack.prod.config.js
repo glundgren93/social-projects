3c1f00230c42ba3f605f75997b422eb883efc989
Gabriel Lundgren <glundgren93@gmail.com>
2016-11-04 13:37:09 -0200

    changing index

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,33 +1,40 @@
 var webpack = require('webpack');
 
 module.exports = {
-  entry: {
-    main: './src/index.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel'
+    devtool: 'source-map',
+    entry: {
+        main: './src/index.js'
     },
-    {
-      test: /\.scss$/,
-      loader: 'style!css!sass'
-    }]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': '"production"'
-      }
-    })
-  ]
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'react-hot!babel'
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css!sass'
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
 };
