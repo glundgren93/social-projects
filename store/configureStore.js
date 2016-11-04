69916beac1a9f14e55696c45e1b5421c64d70207
Gabriel Lundgren <glundgren93@gmail.com>
2016-11-04 13:50:24 -0200

    removing logger from store

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,17 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import { browserHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
-import createLogger from 'redux-logger';
 import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
 import localStorageLoad from '../middleware/localStorageLoad';
 import localStorageDump from '../middleware/localStorageDump';
 
-const logger = createLogger();
 const router = routerMiddleware(browserHistory);
 
-const createStoreWithMiddleware = applyMiddleware(localStorageLoad, ReduxPromise, thunk, router, localStorageDump, logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(localStorageLoad, ReduxPromise, thunk, router, localStorageDump)(createStore);
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleware(rootReducer, initialState);
