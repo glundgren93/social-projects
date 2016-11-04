b006e90b893bb8c972dee3871ab8a582d22ac502
Gabriel Lundgren <glundgren93@gmail.com>
2016-11-04 10:18:11 -0200

    update webpack.prod.config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -24,15 +24,6 @@ module.exports = {
     }]
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': '"production"'
