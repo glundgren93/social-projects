df4dc8d6ff1c2b40724182b28cd715d071894666
Gabriel Lundgren <glundgren93@gmail.com>
2016-11-09 22:20:08 -0200

    changing project component style

--- src/components/Project/presenter.js
+++ src/components/Project/presenter.js
@@ -3,8 +3,8 @@ import 'rc-collapse/assets/index.css';
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Jumbotron from '../Jumbotron';
-import Collapse from 'rc-collapse';
 import ReactDisqus from '../ReactDisqus';
+import Footer from '../Footer';
 
 class Project extends Component {
   componentWillMount() {
@@ -13,49 +13,80 @@ class Project extends Component {
 
   render() {
     const { project } = this.props;
-    const Panel = Collapse.Panel;
 
-    const collapse = (
+    const contacts = (
       project && project.organizer && project.institution ?
-      <Collapse accordion={false}>
-        <Panel className="bold" header="O que?">{project.description}</Panel>
-        <Panel header="Quando?">{project.date}</Panel>
-        <Panel header="Onde?">{project.location}</Panel>
-        <Panel header="Contato da Instituição">
-          <ul>
-            <li><span className="glyphicon glyphicon-user" aria-hidden="true"> </span> { project.institution.name  }</li>
-            <li><span className="glyphicon glyphicon-envelope" aria-hidden="true"> </span> { project.institution.email  }</li>
-            <li><span className="glyphicon glyphicon-phone" aria-hidden="true"> </span> { project.institution.phone  }</li>
-          </ul>
-        </Panel>
-        <Panel header="Organizador da Ação">
-          <ul>
-            <li><span className="glyphicon glyphicon-user" aria-hidden="true"> </span> { project.organizer.name  }</li>
-            <li><span className="glyphicon glyphicon-envelope" aria-hidden="true"> </span> { project.organizer.email  }</li>
-            <li><span className="glyphicon glyphicon-phone" aria-hidden="true"> </span> { project.organizer.phone  }</li>
-          </ul>
-        </Panel>
-      </Collapse>
+      <div className="col-md-4">
+        <div className="widget-wrapper">
+          <h4>Contato da Instituição:</h4>
+          <br/>
+          <div className="card">
+              <div className="card-block">
+                  <p><span><i className="fa fa-user prefix"></i> { project.institution.name  }</span></p>
+                  <p><span><i className="fa fa-envelope prefix"></i> { project.institution.email  }</span></p>
+                  <p><span><i className="fa fa-phone prefix"></i> { project.institution.phone  }</span></p>
+              </div>
+          </div>
+        </div>
+        <div className="widget-wrapper">
+          <h4>Organizador da Ação:</h4>
+          <br/>
+          <div className="card">
+                <div className="card-block">
+                  <p><span><i className="fa fa-user prefix"></i> { project.organizer.name  }</span></p>
+                  <p><span><i className="fa fa-envelope prefix"></i> { project.organizer.email  }</span></p>
+                  <p><span><i className="fa fa-phone prefix"></i> { project.organizer.phone  }</span></p>
+                </div>
+          </div>
+        </div>
+      </div>
       : 'Carregando'
     );
 
     return (
-      <div className="container-fluid ">
-       <div className="row">
-          <div className="page-header text-center">
-            <h1>{project.name} </h1>
-          </div>
-          <div className="col-md-2">
-            <div className="project-media">
-                <img className="img-thumbnail" src={project.imgPath} alt="..." />
+      <div>
+      <main>
+        <div className="container">
+          <div className="row">
+            {contacts}
+            <div className="col-md-8">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="product-wrapper">
+                    <div className="view overlay hm-white-light z-depth-1-half">
+                      <img src={project.imgPath} className="img-fluid" alt=""/>
+                      <div className="mask">
+                      </div>
+                    </div>
+                    <br/>
+                    <h2 className="h2-responsive">{project.name}</h2>
+                    <hr/>
+                    <p><span><i className="fa fa-info-circle"></i> {project.description}</span></p>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="reviews">
+                    <h2 className="h2-responsive">Quando?</h2>
+                    <hr/>
+                    <p><span><i className="fa fa-calendar"></i> {project.date}</span></p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="reviews">
+                    <h2 className="h2-responsive">Onde?</h2>
+                    <hr/>
+                    <p><span><i className="fa fa-location-arrow"></i> {project.location}</span></p>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col-md-10">
-            {collapse}
-          </div>
         </div>
+
         <hr/>
         <ReactDisqus shortname="socialprojects" identifier={project.id} />
+      </main>
+      <Footer />
       </div>
     );
   }
