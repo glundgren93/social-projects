b3c83b93f624845ce03696e5397487cf4a81d811
Gabriel Lundgren <glundgren93@gmail.com>
2016-11-12 10:52:51 -0200

    using webpack to render css

--- src/components/ProjectListItem/index.js
+++ src/components/ProjectListItem/index.js
@@ -3,19 +3,21 @@ import { Link } from 'react-router';
 
 const ProjectListItem = ({ project }) => {
     return (
-      <div className="col-md-3">
-        <div className="card wow fadeInUp">
-            <div className="view overlay hm-white-slight center-on-small-only">
-                <img src={ project.imgPath } className="rounded-circle img-fluid" alt="" />
-            </div>
-            <div className="card-block text-xs-center">
-                <h4 className="card-title">{project.name}</h4>
-                <hr/>
-                <p className="card-text">{ project.briefDescription }</p>
-                <Link className="btn btn-primary" to={`/project/${project.id}`} key={ project.id }>Veja mais</Link>
-            </div>
-        </div>
-    </div>
+      <div className="col-md-3 project-item">
+        <Link to={`/project/${project.id}`} key={ project.id }>
+          <div className="card wow fadeInUp">
+              <div className="view overlay hm-white-slight center-on-small-only">
+                  <img src={ project.imgPath } className="rounded-circle img-fluid" alt="" />
+                  <div className="mask"></div>
+              </div>
+              <div className="card-block text-xs-center">
+                  <h4 className="card-title">{project.name}</h4>
+                  <hr/>
+                  <p className="card-text">{ project.briefDescription }</p>
+              </div>
+          </div>
+        </Link>
+      </div>
     )
 }
 export default ProjectListItem;
